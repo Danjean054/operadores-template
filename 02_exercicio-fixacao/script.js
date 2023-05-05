cb9f606cfcd5c8251dbfd15a83c56108542eb21c
Daniel Weslley Silva Xavier <danielweslley882@gmail.com>
2023-05-04 21:09:16 -0300

    Aprendendo a usar operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,14 @@
 // EXERCICIO DE FIXAÇÃO
 
+let idade = Number(prompt('Digita sua idade'));
+
+let idade2 = Number(prompt('Qual é a idade do seu amigo(a)?'));
+
+//console.log(`Sua idade é maior que a do seu melhor amigo(a)? ${idade > idade2}`);
+
+console.log('Sua idade é maior que a do seu melhor amigo(a)?', idade > idade2);
+
+//console.log(`A diferaça da sua idade para do seu melhor amigo(a) é de ${idade - idade2} anos.`)
+
+console.log('A diferaça da sua idade para do seu melhor amigo(a) é de',idade - idade2, 'anos.')
+
